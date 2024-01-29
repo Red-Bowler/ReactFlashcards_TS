4bf6e5393f089ec5d82fda99c38aed9e629eacef
Red-Bowler <109992108+Red-Bowler@users.noreply.github.com>
2024-01-29 17:44:47 -0300

    appcontext to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Header from "./components/Header/Header.js";
+// import Main from "./components/Main.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context/AppContext";
+import svg from "./svg/cherry-blossom-petal.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const github = (
+        <span>
+            Ve este projecto en{" "}
+            <a
+                href="https://github.com/owaruuu/ReactFlashcards"
+                target="_blank"
+            >
+                Github
+            </a>
+            .
+        </span>
+    );
+
+    const linkedin = (
+        <span>
+            © por{" "}
+            <a
+                href="https://www.linkedin.com/in/josuemarquez/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Josue Marquez
+            </a>{" "}
+            2023.
+        </span>
+    );
+
+    return (
+        <AppProvider>
+            <div className="App">
+                {/* <Header /> */}
+                {/* <hr style={{ margin: 0 }}></hr> */}
+                {/* <Main /> */}
+                <div className="divider">
+                    <img className="logo" src={svg}></img>
+                </div>
+                <footer>
+                    {github}
+                    {linkedin}
+                </footer>
+            </div>
+        </AppProvider>
+    );
 }
 
 export default App;
---
+++ src/context/AppContext.tsx
@@ -0,0 +1,161 @@
+import { createContext, useReducer } from "react";
+import { LectureType, lectures } from "../data/lectures";
+
+// interface ProgressType = {
+//     [key: number]?: {[key: number]: any},
+//     stickers: {[key: number]: any}
+// };
+type ProgressType = any; // TODO define type
+
+type StateType = {
+    init: Boolean, //true despues de haber intentado conectarse a cognito
+    cognitoError: Boolean, //para fallas con el servicio de cognito
+    cognito: Boolean,
+    serverError: Boolean, //para fallas con mi server de Render.com
+    loaded: Boolean,
+    loggedIn: Boolean, //true si ya confirme que tengo tokens validos
+    loginControlErrorMessage: String,
+    lectures: LectureType[],
+    gotLectures: Boolean,
+    user: {
+        userName: String,
+        currentProgress: ProgressType | null,
+    },
+    appState: { currentScreen: String, currentLecture: String | null, lastScreen: String | null }, //currentLecture es el id
+    needToSave: Boolean,
+    isTakingTest: Boolean,
+    savedTest: Boolean,
+    saveError: Boolean,
+    saveInfoMessage: String,
+    dbError: Boolean,
+};
+
+type ActionType = {
+    type: String, 
+    payload: any
+};
+
+
+export const AppReducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+        case "SET_INIT":
+            return { ...state, init: action.payload };
+        case "SET_COGNITO_ERROR":
+            return { ...state, cognitoError: action.payload };
+        case "SET_SERVER_ERROR":
+            return { ...state, serverError: action.payload };
+        case "SET_DB_ERROR":
+            return { ...state, dbError: action.payload };
+        case "SET_LOADED":
+            return { ...state, loaded: action.payload };
+        case "SET_LOG_STATUS":
+            return { ...state, loggedIn: action.payload };
+        case "SET_LOGIN_CONTROL_MSG":
+            return { ...state, LoginControlErrorMessage: action.payload };
+        case "SET_LECTURES":
+            return { ...state, lectures: action.payload };
+        case "SET_LECTURES_FLAG":
+            return { ...state, gotLectures: action.payload };
+        case "SET_USER":
+            return {
+                ...state,
+                user: { ...state.user, ...action.payload },
+            };
+        case "CHANGE_SCREEN":
+            return {
+                ...state,
+                appState: {
+                    ...state.appState,
+                    ...action.payload,
+                    lastScreen:
+                        state.appState.currentScreen ===
+                        action.payload.currentScreen
+                            ? state.appState.lastScreen
+                            : state.appState.currentScreen,
+                },
+            };
+        case "UPDATE_PROGRESS":
+            return {
+                ...state,
+                user: { ...state.user, ...action.payload },
+                needToSave: true,
+            };
+        case "SET_SAVE_FLAG":
+            return { ...state, needToSave: action.payload };
+        case "SET_IS_TAKING_TEST":
+            return { ...state, isTakingTest: action.payload };
+        case "SET_SAVE_TEST":
+            return { ...state, savedTest: action.payload };
+        case "SET_SAVE_ERROR":
+            return { ...state, saveError: action.payload };
+        case "SET_SAVE_INFO_MSG":
+            return { ...state, saveInfoMessage: action.payload };
+        default:
+            throw new Error("wrong action type: " + action.type);
+    }
+};
+
+//Crear un initial state leyendo de la base de datos o localStorage
+const initialState: StateType = {
+    init: false, //true despues de haber intentado conectarse a cognito
+    cognitoError: false, //para fallas con el servicio de cognito
+    cognito: false,
+    serverError: false, //para fallas con mi server de Render.com
+    loaded: false,
+    loggedIn: false, //true si ya confirme que tengo tokens validos
+    loginControlErrorMessage: "",
+    lectures: lectures,
+    gotLectures: false,
+    user: {
+        userName: "",
+        currentProgress: null,
+    },
+    appState: { currentScreen: "main", currentLecture: null, lastScreen: null }, //currentLecture es el id
+    needToSave: false,
+    isTakingTest: false,
+    savedTest: false,
+    saveError: false,
+    saveInfoMessage: "",
+    dbError: false,
+};
+
+
+
+export const AppContext = createContext<[StateType, React.Dispatch<ActionType>]>([
+    initialState,
+    () => {},
+]);
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export const AppProvider = (props: Props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+        <AppContext.Provider
+            value={[{
+                init: state.init,
+                cognitoError: state.cognitoError,
+                cognito: state.cognito,
+                serverError: state.serverError,
+                loaded: state.loaded,
+                loggedIn: state.loggedIn,
+                loginControlErrorMessage: state.loginControlErrorMessage,
+                lectures: state.lectures,
+                gotLectures: state.gotLectures,
+                user: state.user,
+                appState: state.appState,
+                needToSave: state.needToSave,
+                isTakingTest: state.isTakingTest,
+                savedTest: state.savedTest,
+                saveError: state.saveError,
+                saveInfoMessage: state.saveInfoMessage,
+                dbError: state.dbError,
+            }, dispatch]}
+        >
+            {props.children}
+        </AppContext.Provider>
+    );
+};
